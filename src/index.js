f8159199a9dbeb9afad73b61d259c2c1b4283500
Pony Hsieh <lbjwin@gmail.com>
2023-06-02 10:28:28 +0800

    feat: 新增 gh-pages 部屬相關設定

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/youbike-2.0-taipei'>
       <App />
     </BrowserRouter>
   </Provider>
